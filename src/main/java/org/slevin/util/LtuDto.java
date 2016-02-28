96c71bd79c6f8ae40fda73186f01dd6f3df9060b
Ramazan FIRIN <ramazan_firin@hotmail.com>
2016-02-28 15:27:03 +0200

    dfdf

---
+++ src/main/java/org/slevin/util/LtuDto.java
@@ -0,0 +1,21 @@
+package org.slevin.util;
+
+public class LtuDto {
+
+	private String id;
+	private String score;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+}
